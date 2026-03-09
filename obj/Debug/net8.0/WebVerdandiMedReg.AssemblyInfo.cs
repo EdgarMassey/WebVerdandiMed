ec710b63c3d5331bf4b0f6142cb6776f92925c0d
EdgarMassey <58510315+EdgarMassey@users.noreply.github.com>
2026-03-09 18:58:12 +0100

    20260212a
    Signed-off-by: EdgarMassey <58510315+EdgarMassey@users.noreply.github.com>

--- obj/Debug/net8.0/WebVerdandiMedReg.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebVerdandiMedReg.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebVerdandiMedReg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93bd6be02f2b6088c510d4bbf2d378105f611008")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fbb90be1e215d4df1c61d5770de4fdd62011ec8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebVerdandiMedReg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebVerdandiMedReg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
